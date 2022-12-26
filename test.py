f0f6f0d11bce1891b21f81eab3e01e884a95d97c
cubecloud <rainmaverick@mail.ru>
2022-12-26 19:57:36 +0300

    Testing

--- test.py
+++ test.py
@@ -1,21 +1,26 @@
-#!/usr/bin/env python3
+import os
+import sys
 from btbinance import BinanceStore
 
-api_key = "a7cc06cad7f1f08c8454a3f2ef0886490ae12a2ff3ec3184287bccf7c1207570"
-api_secret = "9da25f276b0bfc1d35720ec047cbbafc1f979426888790d1b930db094d42c4d8"
+path_head = os.path.abspath('..')
+sys.path.append(path_head)
+print(path_head)
+
+from sunday.datawizard.secure import Secure
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+""" Decrypt key and secret """
+secure_key = Secure()
+__key, __secret = secure_key.get_key()
 
 
 def main():
-    store = BinanceStore(config=dict(
-        apiKey=api_key,
-        secret=api_secret,
-        options={'defaultType': 'future'},
-    ),
-                         sandbox=True)
-    # store.subscribe_account()
-    # store.subscribe_bars(['btcusdt'], '5m')
-    # store._t_loop_stream()
+    store = BinanceStore(crrency="BNBBTC",
+                         key=__key,
+                         secret=__secret,
+                         ),
 
-
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
